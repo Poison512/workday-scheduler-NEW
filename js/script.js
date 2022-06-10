c864f01c3e77118efa1a313421a48c6b568afbc4
Adrian Alaniz <adrian512@Adrians-MBP.attlocal.net>
2022-06-10 12:50:44 -0500

    update whole file

--- js/script.js
+++ js/script.js
@@ -1,9 +1,44 @@
+$(document).ready(function() {
+    $("#momento").text (moment().format("MMMM Do YY, h:mm:ss a"));
+    $(".saveBtn").on("click",function() {
 
-var today = new Date();
-var day = today.getDay();
-var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear()
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
- 
-document.getElementById("rightNowDisplay").innerHTML = dateTime+ " " + daylist[day];
+console.log(this);
+var text = $(this).siblings(".description").val();
+var time = $(this).parent().attr("id");
+localStorage.setItem(text, time);
+    })
+$('#hour1 .description').val(localStorage.getItem('hour1')) ;
+$('#hour2 .description').val(localStorage.getItem('hour2')) ;
+$('#hour3 .description').val(localStorage.getItem('hour3')) ;
+$('#hour4 .description').val(localStorage.getItem('hour4')) ;
+$('#hour5 .description').val(localStorage.getItem('hour5')) ;
+$('#hour6 .description').val(localStorage.getItem('hour6')) ;
+$('#hour7 .description').val(localStorage.getItem('hour7')) ;
+$('#hour8 .description').val(localStorage.getItem('hour8')) ;
+$('#hour9 .description').val(localStorage.getItem('hour9')) ;
+$('#hour10 .description').val(localStorage.getItem('hour10')) ;
+function currentTime(){
+    var ahorita = moment().hour();
+    $('.time-block').each(function () {
+        var timeBlock=parseint($(this).attr('id').split('hour')[1])
+        console.log(timeBlock, ahorita)
+        if (ahorita > timeBlock){
+            $(this).addClass('past');
+            $(this).removeClass('future');
+            $(this).removeClass('present');
+        }
+        else if (ahorita === timeBlock){
+            $(this).removeClass('past');
+            $(this).addClass('present');
+            $(this).removeClass('future');
+        }
+        else {
+            $(this).removeClass('present');
+            $(this).removeClass('past');
+            $(this).addClass('future');
+        }
+    }
+    )
+}
+currentTime();
+})
